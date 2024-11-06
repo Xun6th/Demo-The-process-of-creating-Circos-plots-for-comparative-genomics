bd7451a04a50caefe31e3ca46c65514b136dc823
ChengCh'i  Wang <160301876+Xun6th@users.noreply.github.com>
2024-11-06 18:36:16 +0800

    Add files via upload

--- circos.sh
+++ circos.sh
@@ -70,50 +70,6 @@ awk '{print > $12 "_part_" FILENAME}' to_pav.raw
 awk -F'\t' '{print $4}' To_Chr1_part_to_pav.raw > 1
 sort -n 1 > 1_sort
 python ~/hang.py 1_sort 1_sort_merge
-cat ~/hang.py
-#!/usr/bin/env python3
-import argparse
-
-def process_file(input_file, output_file):
-    with open(input_file, 'r') as infile:
-        numbers = [int(line.strip()) for line in infile]
-
-    if not numbers:
-        return
-
-    groups = []
-    current_group = [numbers[0]]
-
-    for i in range(1, len(numbers)):
-        if numbers[i] == numbers[i - 1] + 1:
-            current_group.append(numbers[i])
-        else:
-            groups.append(current_group)
-            current_group = [numbers[i]]
-
-    groups.append(current_group)  # Append the last group
-
-    with open(output_file, 'w') as outfile:
-        for group in groups:
-            if len(group) > 1:
-                outfile.write(f"{group[0]}\t{group[-1]}\n")
-            else:
-                outfile.write(f"{group[0]}\n")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process a file to merge consecutive numbers.")
-    parser.add_argument("input_file", help="Input file path")
-    parser.add_argument("output_file", help="Output file path")
-
-    args = parser.parse_args()
-    process_file(args.input_file, args.output_file)
-
-
-
-
-
-
-
 awk -F'\t' 'NF >= 2' 1_sort_merge > 1_sort_merge_filter
 awk '{print "To_Chr1" "\t" $0}' 1_sort_merge_filter > 1_chr
 cat 1_chr 2_chr 3_chr 4_chr 5_chr > td_pav.bed
